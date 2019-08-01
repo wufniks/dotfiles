d59cb8208d6e39e286254592c9bf090f7b1bf466
Heejae Shin <hshin@nielsen.com>
2019-08-02 01:21:58 +0900

    update configuration
    * install tig, emacs-plus cascadia font, doom emacs

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@
 # install rustup
 curl https://sh.rustup.rs -sSf | sh
 
-brew install pyenv pyenv-virtualenv zsh starship z
+brew install pyenv pyenv-virtualenv zsh starship z tig
 
 # https://wesleywiser.github.io/post/vscode-vim-repeat-osx/
 # repeat key press in vim mode in vscode
@@ -20,3 +20,11 @@ ln -s $(pwd)/omz/hshin.zsh ~/.oh-my-zsh/custom/
 
 # install zinit
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+
+# install cascadia font
+brew tap homebrew/cask-fonts && brew cask install font-cascadia-nerd-font
+
+# install emacs-plus
+brew tap d12frosted/emacs-plus && brew install emacs-plus
+git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
