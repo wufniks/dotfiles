86c2a3331b2bca779b3a4816d320b0cfc3d42f64
Heejae Shin <hshin@nielsen.com>
2020-05-19 10:37:18 +0900

    Update config

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -71,7 +71,7 @@
        :term
        ;;eshell            ; a consistent, cross-platform shell (WIP)
        ;;shell             ; a terminal REPL for Emacs
-       ;;term              ; terminals in Emacs
+       term              ; terminals in Emacs
        ;;vterm             ; another terminals in Emacs
 
        :checkers
@@ -97,7 +97,7 @@
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
-       ;;terraform         ; infrastructure as code
+       terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,8 @@
 # install rustup
 curl https://sh.rustup.rs -sSf | sh
 
-brew install pyenv pyenv-virtualenv zsh starship z tig
+# brew install pyenv pyenv-virtualenv zsh starship z tig
+brew bundle
 
 # https://wesleywiser.github.io/post/vscode-vim-repeat-osx/
 # repeat key press in vim mode in vscode
@@ -21,10 +22,10 @@ ln -s $(pwd)/omz/hshin.zsh ~/.oh-my-zsh/custom/
 # install zinit
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
-# install cascadia font
-brew tap homebrew/cask-fonts && brew cask install font-cascadia-nerd-font
+# # install cascadia font
+# brew tap homebrew/cask-fonts && brew cask install font-cascadia-nerd-font
 
 # install emacs-plus
-brew tap d12frosted/emacs-plus && brew install emacs-plus
+# brew tap d12frosted/emacs-plus && brew install emacs-plus
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
