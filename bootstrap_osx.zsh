c3a1c3953104bed746ac0752986cc8ee8d4c55a4
신희제 <robin@aladin.local>
2021-11-22 23:12:10 +0900

    replace init script for zinit

--- bootstrap_osx.zsh
+++ bootstrap_osx.zsh
@@ -42,7 +42,7 @@ main() {
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 	# install zinit
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma-continuum/zinit/master/doc/install.sh)"
+	sh -c "$(curl -fsSL https://git.io/zinit-install)"
 
 	cat $DOTFILES_DIR/zshrc >> $(HOME)/.zshrc
 
