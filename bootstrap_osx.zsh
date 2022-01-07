1be351f3e630338decf4442b6b3cb7d069110fe7
신희제 <hshin@sirius.local>
2022-01-07 17:03:20 +0900

    basic error handling

--- bootstrap_osx.zsh
+++ bootstrap_osx.zsh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
 set -euo pipefail
 
-DOTFILES_DIR=$HOME/.dotfiles
+DOTFILES_DIR="${HOME}/.dotfiles"
+ITERM_COLOR_SCHEME="${DOTFILES_DIR}/iTerm2-Color-Schemes"
 
 install_homebrew() {
     if ! type brew > /dev/null; then
@@ -18,7 +19,7 @@ prepare() {
 
 install_formulas() {
     # brew install pyenv pyenv-virtualenv zsh starship z tig
-    brew bundle --file $DOTFILES_DIR/Brewfile
+    brew bundle --file ${DOTFILES_DIR}/Brewfile
 }
 
 configure() {
@@ -30,10 +31,16 @@ configure() {
 main() {
     prepare
 
-    git clone https://github.com/wufniks/dotfiles.git $DOTFILES_DIR || true
+    if [ ! -d ${DOTFILES_DIR} ]; then
+        git clone https://github.com/wufniks/dotfiles.git ${DOTFILES_DIR} --recursive
+    fi
 
-    cp $DOTFILES_DIR/.zshenv $(HOME)/.zshenv
-    ln -s $DOTFILES_DIR/config $(HOME)/.config
+    cp ${DOTFILES_DIR}/.zshenv ${HOME}/.zshenv
+
+    if [ ! -d ${HOME}/.config ]; then
+        mv ${HOME}/.config ${HOME}/.config.bak
+    fi
+    ln -s ${DOTFILES_DIR}/config ${HOME}/.config
 
     install_formulas
 
@@ -54,17 +61,22 @@ main() {
     ~/.emacs.d/bin/doom install
 
     # color themes
-    git clone https://github.com/mbadolato/iTerm2-Color-Schemes.git $DOTFILES_DIR/
+    if [ ! -d $ITERM_COLOR_SCHEME ]; then
+    	git clone https://github.com/mbadolato/iTerm2-Color-Schemes.git ${ITERM_COLOR_SCHEME}
+    fi
+    echo "Installed iterm2 color scheme"
 
     # install vim-plug
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    echo "Installed vim-plug"
 
     ln -s $DOTFILES_DIR/vimrc $HOME/.vimrc
     mkdir -p $HOME/.config/nvim
     ln -s $DOTFILES_DIR/nvimrc $HOME/.config/nvim/init.vim
+    echo "Installed vim configuration"
 
     configure
 }
